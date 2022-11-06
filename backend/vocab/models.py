9176e50151fcffef21f171498a7f36beeb4a8843
Yizhe Wang <will.yizhewang@gmail.com>
2022-11-05 21:27:21 -0700

    fix: fix many to many relations

--- backend/vocab/models.py
+++ backend/vocab/models.py
@@ -6,8 +6,6 @@ from django.db import models
 class Word(models.Model):
     word = models.CharField(max_length=100)
     definition = models.CharField(max_length=1000)
-    example_sentences = models.ManyToManyField(
-        Sentence, through="WordSentence")
     img_url = models.CharField(max_length=1000, default="")
     # word_type = models.CharField(max_length=100)
     # synonyms = models.CharField(max_length=1000)
@@ -21,17 +19,7 @@ class Word(models.Model):
 
 class Sentence(models.Model):
     sentence = models.CharField(max_length=1000)
-    words = models.ManyToManyField(Word, through='WordSentence')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.sentence
-
-
-class WordSentence(models.Model):
-    word = models.ForeignKey(Word)
-    sentence = models.ForeignKey(Sentence)
+    words = models.ManyToManyField(Word, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -40,8 +28,8 @@ class WordSentence(models.Model):
 
 
 class User(models.Model):
-    saved_words = models.ManyToManyField(Word, through='UserWord')
-    saved_sentences = models.ManyToManyField(Sentence, through='UserSentence')
+    saved_words = models.ManyToManyField(Word, blank=True)
+    saved_sentences = models.ManyToManyField(Sentence, blank=True)
     username = models.CharField(max_length=100)
     email = models.CharField(max_length=1000)
     password = models.CharField(max_length=100)
@@ -50,23 +38,3 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
-
-
-class UserWord(models.Model):
-    user = models.ForeignKey(User)
-    word = models.ForeignKey(Word)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.word
-
-
-class UserSentence(models.Model):
-    user = models.ForeignKey(User)
-    sentence = models.ForeignKey(Sentence)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.sentence
